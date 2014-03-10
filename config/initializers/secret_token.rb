08628ff40b6eb6a4b370f8e13496a6886f6c232c
vagrant <vagrant@ironhack>
2014-03-10 14:29:30 +0000

    first commit - Rails week 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ironevents::Application.config.secret_key_base = '3e5da4a68f76505249c5c108e827ac3497ee1181eb6b9ca0401b6f660d6096fc45ea71a1844f3f301829ff8298a7cf348fcb41b375275ba9a0c6a3a8a710808b'
